c70e4fb41902dfd27ced430568b4753247772097
ZyCromerZ <neetroid97@gmail.com>
2023-03-08 02:08:32 +0700

    update
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- get-proxy.sh
+++ get-proxy.sh
@@ -1,2 +1,4 @@
-proxybroker find --types HTTP HTTPS SOCKS4 SOCKS5 -l 700 -o get.txt
+proxybroker find --types HTTP HTTPS SOCKS4 SOCKS5 -l 40 -c ID -o get.txt
 cat get.txt | awk -F "] " '{print $2}' | awk -F ">" '{print $1}' > result.txt
+proxybroker find --types HTTP HTTPS SOCKS4 SOCKS5 -l 700 -o get.txt
+cat get.txt | awk -F "] " '{print $2}' | awk -F ">" '{print $1}' >> result.txt
